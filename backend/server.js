a09c315694149a06356a7d14f515bd7486af4e9f
kalpesh0711 <kalpeshchoudhary202@gmail.com>
2026-02-22 01:05:23 +0530

    Day 52: Connected MongoDB Atlas using Mongoose and environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
+const connectDB = require("./config/db");   // ADD THIS
 const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
+// CONNECT DATABASE
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/products",productRoutes);
+app.use("/api/products", productRoutes);
 
-app.get("/",(req,res)=> {
-    res.send("Shopsphere Backend Running");
+app.get("/", (req, res) => {
+  res.send("Shopsphere Backend Running");
 });
 
+// Port
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> {
-    console.log(`server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
